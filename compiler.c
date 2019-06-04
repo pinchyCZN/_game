d93ab3dad5545f1a51f5d63d1224fe50fab540f6
pinchyCZN <pinchyCZN@gmail.com>
2019-06-04 16:05:40 -0500

    updates

--- compiler.c
+++ compiler.c
@@ -1,45 +1,37 @@
-/*
-* Simple Test program for libtcc
-*
-* libtcc can be useful to use tcc as a "backend" for a code generator.
-*/
+#include <allegro5/allegro.h>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "libtcc.h"
 
-/* this function is called by the generated code */
+ALLEGRO_THREAD *g_compiler_thread;
+
+
 int add(int a, int b)
 {
 	return a + b;
 }
 
-/* this strinc is referenced by the generated code */
 const char *hello()
 {
 	return "hello!\n";
 }
 
+int my_printf(const char *fmt,...)
+{
+	return 0;
+}
+
 char my_program[] =
-"#include <tcclib.h>\n" /* include the "Simple libc header for TCC" */
-"extern int add(int a, int b);\n"
-"extern const char *hello();\n"
-"int fib(int n)\n"
-"{\n"
-"    if (n <= 2)\n"
-"        return 1;\n"
-"    else\n"
-"        return fib(n-1) + fib(n-2);\n"
-"}\n"
+"#include <allegro5/allegro.h>\n"
+"int func(int x){\n"
+"return 0\n"
 "\n"
-"int foo(int n)\n"
-"{\n"
-"    printf(\"%s\\n\", hello());\n"
-"    printf(\"fib(%d) = %d\\n\", n, fib(n));\n"
-"    printf(\"add(%d, %d) = %d\\n\", n, 2 * n, add(n, 2 * n));\n"
-"    return 0;\n"
-"}\n";
+"}\n"
+;
+
 
 int test_shit()
 {
@@ -67,8 +59,11 @@ int test_shit()
 				tcc_add_library_path(s, a+2);
 		}
 	}
-	tcc_add_include_path(s,"c:\\DEV\\MSVC_projects\\_game\\tcc\\");
-	tcc_add_include_path(s,"c:\\DEV\\MSVC_projects\\_game\\tcc\\src\\include\\");
+	tcc_add_include_path(s,"..\\tcc\\");
+	tcc_add_include_path(s,"..\\tcc\\include\\");
+	tcc_add_include_path(s,"..\\allegro\\include");
+	tcc_add_include_path(s,"..\\tcc\\win32\\include");
+	tcc_add_library_path(s,"..\\tcc\\win32\\lib");
 
 	/* MUST BE CALLED before any compilation */
 	tcc_set_output_type(s, TCC_OUTPUT_MEMORY);
@@ -80,7 +75,7 @@ int test_shit()
 	You may also open a dll with tcc_add_dll() and use symbols from that */
 	tcc_add_symbol(s, "add", add);
 	tcc_add_symbol(s, "hello", hello);
-	tcc_add_symbol(s, "printf", printf);
+	tcc_add_symbol(s, "printf", my_printf);
 
 	/* relocate the code */
 	if (tcc_relocate(s, TCC_RELOCATE_AUTO) < 0){
@@ -101,4 +96,17 @@ int test_shit()
 	tcc_delete(s);
 
 	return 0;
+}
+
+void *compile_thread(ALLEGRO_THREAD *athread,void *arg)
+{
+
+	return 0;
+}
+
+int start_compiler_thread()
+{
+	g_compiler_thread=al_create_thread(&compile_thread,0);
+	al_start_thread(g_compiler_thread);
+	return TRUE;
 }
